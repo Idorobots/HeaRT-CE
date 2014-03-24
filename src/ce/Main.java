2fe7910c5e16fdaa6e732cab3f3eebd3c186b6ba
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2014-03-24 19:21:30 +0100

    Added a sample JHeroic call to check if things are worknig.
    ...they are not. Obviously.

--- src/ce/Main.java
+++ src/ce/Main.java
@@ -1,7 +1,20 @@
 package ce;
 
+import JHeroic.JHeroic;
+import JHeroic.JHModel;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello HeaRT!");
+
+        JHeroic jh = new JHeroic(1, "localhost", 8090);
+
+        try {
+            for(JHModel m : jh.getModelList()) {
+                System.out.println(m.getReplayString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
